8829c4498728eff215e2bc6919368fc8ae9b3ee0
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-01-16 22:15:17 -0300

    Teste de um servidor Apache com PHP 8.0

---
+++ test-php/index.php
@@ -0,0 +1,17 @@
+<?php
+    $string = "Meu teste em PHP deu certo";
+    $number = rand();
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Teste em PHP</title>
+</head>
+<body>
+    <h1><? echo $string; ?></h1>
+    <p>Deu certo sim!!</p>
+    <h3>Esse é um número aleatório: <? echo $number; ?></h3>
+</body>
+</html>
